576f214d8263f478ab83b2078561f63192e32dd1
pedroMartinsMJ <pedrohmrs2004@gmail.com>
2025-05-22 22:41:14 -0300

    programação defenciva, melhora no relacionamento entidades

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -7,31 +7,54 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int columns, int rows) {
+        if (rows < 1 || columns < 1){
+            throw new BoardException("erro criado tabuleiro, sem linha e coluna");
+        }
         this.columns = columns;
         this.rows = rows;
         this.pieces = new Piece[rows][columns];
     }
 
-    public int getRows() {
-        return rows;
-    }
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
+    public int getRows() { return rows; }
     public int getColumns() {
         return columns;
-    }
-    public void setColumns(int columns) {
-        this.columns = columns;
     }
 
     //retorna a peça na localização x da matriz
     public Piece piece(int row, int column) {
+        if (!positionExists(row,column)){
+            throw new BoardException("posição não existe, erro de digitação");
+        }
         return pieces[row][column];
     }
     //sobrecarga
     public Piece piece(Position position) {
+        if (!positionExists(position)){
+            throw new BoardException("posição não existe, erro de digitação");
+        }
         return pieces[position.getRow()][position.getColumn()];
+    }
+
+    public void placePiece(Piece piece, Position position){
+        if (thereIsAPiece(position)){
+            throw new BoardException("existe uma peça nessa posição -> " + position);
+        }
+        this.pieces[position.getRow()][position.getColumn()] = piece;
+        piece.position = position;
+    }
+
+    private boolean positionExists(int row, int column) {
+        return row >=  0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position) {
+        return positionExists(position.getRow(), position.getColumn());
+    }
+
+    public boolean thereIsAPiece(Position position) {
+        if (!positionExists(position)){
+            throw new BoardException("posição não existe, erro de digitação");
+        }
+        return piece(position) != null;
     }
 }
